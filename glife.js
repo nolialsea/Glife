6137d29f5658067c5224191d5359ff5f70746559
nolialsea <nolialsea@hotmail.fr>
2016-07-06 16:47:59 +0200

    Update

--- glife.js
+++ glife.js
@@ -6,11 +6,13 @@
    
   Todo :
    Configurations (local save)
+   Optimisation of the CA
 */
 
 function glifeScope(){ //Just for scope safety
   var listener;
   var started = false;
+  var active = true;
 
   function loadScript(url, callback) {
     var head = document.getElementsByTagName('head') [0];
@@ -23,10 +25,11 @@ function glifeScope(){ //Just for scope safety
   }
 
   function glife() {
-    var speed = 30;
+    var speed = 60;
     var width = prompt('width (default "window.innerWidth")') || window.innerWidth;
     var height = prompt('height (default 16)') || 16;
     var canvas = document.createElement('canvas');
+    var aliveCells = 0.1;
     canvas.id = 'canvasGlife';
     canvas.width = width;
     canvas.height = height;
@@ -42,7 +45,7 @@ function glifeScope(){ //Just for scope safety
     for (var i = 0; i < width; i++) {
       grid1[i] = new Array(height);
       for (var j = 0; j < height; j++) {
-        grid1[i][j] = Math.random() < 0.8 ? false : true;
+        grid1[i][j] = Math.random() > aliveCells ? false : true;
       }
     }
     var grid2 = new Array(width);
@@ -50,6 +53,13 @@ function glifeScope(){ //Just for scope safety
       grid2[i] = new Array(height);
       for (var j = 0; j < height; j++) {
         grid2[i][j] = false;
+      }
+    }
+    var resetGrid = function(){
+      for (var i = 0; i < width; i++) {
+        for (var j = 0; j < height; j++) {
+          grid1[i][j] = Math.random() > aliveCells ? false : true;
+        }
       }
     }
     var drawPoint = function (x, y) {
@@ -103,18 +113,25 @@ function glifeScope(){ //Just for scope safety
     }
     setInterval(function () {
       ctx.clearRect(0, 0, width, height);
-      stepGlife();
-      drawGlife();
+      if (active){
+        stepGlife();
+        drawGlife();
+      }
     }, 1000 / speed);
 
-    listener.sequence_combo('escape f1', function () {
+    listener.sequence_combo('left right left right', function () {
       canvas.style.display = canvas.style.display == "none" ? "" : "none";
+      active = active ? false : true;
+    }, true);
+    
+    listener.sequence_combo('up down up down', function () {
+      resetGrid();
     }, true);
   }
 
   loadScript('https://raw.githubusercontent.com/dmauro/Keypress/development/keypress.js', function () {
     listener = new window.keypress.Listener();
-    listener.sequence_combo('down left up right down left up right escape', function () {
+    listener.sequence_combo('down left up right down left up right', function () {
       if (!started){
         started = true;
         glife();
